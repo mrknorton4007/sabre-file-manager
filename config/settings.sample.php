d7d68ce90612535f340cd59486d9b1f10736c4f3
Mark Norton <mrknorton4007@yahoo.com>
2014-06-10 13:27:50 +0200

    Create settings.sample.php

---
+++ config/settings.sample.php
@@ -0,0 +1,15 @@
+<?php
+	// Sabre config file (example)
+
+	define('sb_site_name', 'Sabre');
+	define('sb_root_path', 'http://www.yourdomain.com/');
+	define('sb_sabre_version', '1.0.0');
+	define('sb_file_path', 'files');
+	define('sb_public_file', 'false');
+	define('sb_password_hash', '21232F297A57A5A743894A0E4A801FC3'); // admin
+	define('sb_user_upload', 'true');
+	define('sb_date_style', 'd/m/Y H:i');
+	define('sb_maintenance_mode', 'false');
+	define('sb_installed', 'true');
+
+?>
