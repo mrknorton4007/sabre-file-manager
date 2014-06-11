523823b458c399becf7dc766d7ebe37ca2893a66
Mark Norton <mrknorton4007@yahoo.com>
2014-06-11 15:01:40 +0200

    Create header.php

---
+++ core/header.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE HTML>
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <title><?php echo sb_site_name; ?> File manager</title>
+      <link rel="stylesheet" type="text/css" href="core/style.css" media="screen" />
+      <script type="text/javascript" src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+   </head>
+   <body>
+      <div id="header">
+         <h1><a id="logo" href="./"><?php echo sb_site_name; ?> File Manager</a></h1>
+      </div> <!-- /header -->
+
+      <div id="page">
+         
