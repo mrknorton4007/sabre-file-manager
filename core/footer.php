5d0f34e48477a13f526ff8c05281fef4a9334ebc
Mark Norton <mrknorton4007@yahoo.com>
2014-06-16 15:09:16 +0200

    Create footer.php

---
+++ core/footer.php
@@ -0,0 +1,7 @@
+      </div> <!-- /page -->
+
+      <div id="footer">
+         <p><a href="http://github.com/mrknorton4007/Sabre">Sabre file manager</a> - version <?php echo sb_version; ?></p>
+      </div> <!-- /footer -->
+   </body>
+</html>
