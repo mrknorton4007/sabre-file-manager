d258b5076ace71304c464a8c55f3312d1494309d
Mark Norton <mrknorton4007@yahoo.com>
2014-08-09 19:31:09 +0200

    Create setup.php

---
+++ core/pages/setup.php
@@ -0,0 +1,10 @@
+<?php
+	include("./core/header.php");
+
+	echo "      <div id=\"setup\" class=\"container\">\n";
+	echo "        "; showMsg('success', 'Thank you for downloading Sabre File Manager');
+	echo "        <p>Sabre is not configured yet. Visit the install folder and follow the on-screen instructions, then reload this page.</p>\n";
+	echo "      </div> <!-- /setup -->\n";
+
+	include("./core/footer.php");
+?>
