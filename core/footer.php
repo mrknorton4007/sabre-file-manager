66ec41c445aafcda07da1190e46ae8053cc49d90
Mark Norton <mrknorton4007@yahoo.com>
2014-08-11 17:56:00 +0200

    Update footer.php

--- core/footer.php
+++ core/footer.php
@@ -2,7 +2,7 @@
 	echo "    </div> <!-- /page -->\n\n";
 	
 	echo "    <div id=\"footer\">\n";
-	echo "      <p><a href=\"https://github.com/mrknorton4007/Sabre\" target=\"_blank\">Sabre file manager</a> - version ".getversion()."</p>\n";
+	echo "      <p><a href=\"https://github.com/mrknorton4007/sabre-file-manager\" target=\"_blank\">Sabre file manager</a> - version ".getversion()."</p>\n";
 	echo "    </div> <!-- /footer -->\n";
 	echo "  </body>\n";
 	echo "</html>";
